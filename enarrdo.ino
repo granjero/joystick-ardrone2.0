dffe80dd8c051dd79f6b23061e6db97e41cb4cd6
jm <jmlombardi@dukarevich.com.ar>
2025-02-06 15:16:57 -0300

    loop control

--- enarrdo.ino
+++ enarrdo.ino
@@ -37,7 +37,7 @@ WiFiUDP udpTelemetry;
 const char* droneIp = "192.168.1.1";
 const int localPort = 8888;
 const int dronePort = 5556;
-const int telemetryPort = 5554;
+//const int telemetryPort = 5554;
 const char* ssid = "enarrdo";
 const char* password = "";
 
@@ -57,6 +57,7 @@ struct DroneCommands {
   const char* setAlarmOk;
   const char* enableNavData;
 };
+
 
 DroneCommands commands = {
   .takeoff = "AT*REF=%d,290718208\r",
@@ -83,6 +84,12 @@ void sendCommand(const char* format, int seq, ...) {
   udp.endPacket();
 }
 
+// convierte un float a 32-bit IEEE 754 integer
+int32_t valorDrone(float value) {
+    int32_t intRep;
+    memcpy(&intRep, &value, sizeof(value));  // Copy raw float bits to int
+    return intRep;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -117,7 +124,6 @@ void setup() {
 }
 
 void loop() {
-  // como estan pullupeados los botones para que tenga sentido los doy vuelta
   bool btn = boton.pressed();
   bool modo = boton_modo.pressed();
   bool arr = up_fwd.pressed();
@@ -125,131 +131,211 @@ void loop() {
   bool izq = left.pressed();
   bool der = right.pressed();
   // volando me interesa el estado y no si fue apretado
+  // como los botones estan pullupeados los doy vuelta para que tenga sentido
   bool btn_state = !boton.read();
   bool modo_state = !boton_modo.read();
   bool arr_state = !up_fwd.read();
   bool aba_state = !dw_bkw.read();
   bool izq_state = !left.read();
   bool der_state = !right.read();
+  
+  static unsigned long tiempo_anterior = 0; // Static variable
+  const unsigned long intervalo = 50; // 50 ms = 20 Hz
+  unsigned long tiempo_actual = millis();
+  // reduzco la velocidad del loop a 20Hz porque el maximo del drone es un poquito mas
+  if (tiempo_actual - tiempo_anterior >= intervalo) {
+    tiempo_anterior = tiempo_actual;
 
-
-  // *******************************
-  // flying
-  // *******************************
-  if (flying) {
-    if (modo_state) {
+    if (flying) {
       // *******************************
-      // MODO apretado
-      if (btn) {
+      // Estamos flying
+      // *******************************
+      if (modo_state) {
         // *******************************
-        // boton apretado
-        debug("boton apretado -> ");
-        flying = false;
-        debug("aterrizar. seq#: ");
-        sendCommand(commands.land, sequenceNumber++);
-        debugln(sequenceNumber);
+        // MODO apretado
+        if (btn) {
+          // boton apretado ********************
+          flying = false;
+
+          debug("volando - > boton apretado -> ");
+          debug("aterrizar. seq#: ");
+          debugln(sequenceNumber);
+
+          sendCommand(commands.land, sequenceNumber++);
+        }
+
+        else if (arr_state) {
+          // boton arriba ******************** 
+          debug("volando - > arriba apretado -> ");
+          debug("Subir. seq#: ");
+          debugln(sequenceNumber);
+          // subir
+          sendCommand(commands.move, sequenceNumber++, 1, 0, 0, valorDrone(0.5), 0);
+        }
+
+        else if (aba_state) {
+          // *******************************
+          // boton abajo
+          debug("volando -> boton abajo apretado -> ");
+          debug("Bajar. seq#: ");
+          debugln(sequenceNumber);
+          // bajar
+          sendCommand(commands.move, sequenceNumber++, 1, 0, 0, valorDrone(-0.5), 0);
+        }
+
+        else if (der_state) {
+          // *******************************
+          // boton derecha
+          debugln("volando -> boton derecha apretado -> ");
+          debug("Rotar Derecha. seq#: ");
+          debugln(sequenceNumber);
+          // rotar derecha
+          sendCommand(commands.move, sequenceNumber++, 1, 0, 0, 0, valorDrone(0.5));
+        }
+
+        else if (izq_state) {
+          // *******************************
+          // boton izquierda
+          debugln("volando -> boton derecha apretado -> ");
+          debug("Rotar Izquierda. seq#: ");
+          debugln(sequenceNumber);
+          // rotar izquierda
+          sendCommand(commands.move, sequenceNumber++, 1, 0, 0, 0, valorDrone(-0.5));
+        }
+
+        else {
+          // *******************************
+          // ningun boton apretado
+          debugln("volando -> ningun botón apretado -> ");
+          debug("Hovering. seq#: ");
+          debugln(sequenceNumber);
+          // HOVER
+          sendCommand(commands.move, sequenceNumber++, 0, 0, 0, 0, 0);
+        }
+
+      } else {
+        // *******************************
+        // MODO suelto
+        if (btn) {
+          // boton apretado ********************
+          flying = false;
+
+          debug("volando - > boton apretado -> ");
+          debug("aterrizar. seq#: ");
+          debugln(sequenceNumber);
+
+          sendCommand(commands.land, sequenceNumber++);
+        }
+
+        else if (arr_state) {
+          // boton arriba ******************** 
+          debug("volando - > arriba apretado -> ");
+          debug("Avanzar. seq#: ");
+          debugln(sequenceNumber);
+          // Avanzar
+          sendCommand(commands.move, sequenceNumber++, 1, 0, valorDrone(0.5), 0 , 0);
+        }
+
+        else if (aba_state) {
+          // *******************************
+          // boton abajo
+          debug("volando -> boton abajo apretado -> ");
+          debug("Retroceder. seq#: ");
+          debugln(sequenceNumber);
+          // bajar
+          sendCommand(commands.move, sequenceNumber++, 1, 0, valorDrone(-0.5), 0 , 0);
+        }
+
+        else if (der_state) {
+          // *******************************
+          // boton derecha
+          debugln("volando -> boton derecha apretado -> ");
+          debug("Rotar Derecha. seq#: ");
+          debugln(sequenceNumber);
+          // mover hacia la derecha
+          sendCommand(commands.move, sequenceNumber++, 1, valorDrone(0.5), 0, 0, 0);
+        }
+
+        else if (izq_state) {
+          // *******************************
+          // boton izquierda
+          debugln("volando -> boton derecha apretado -> ");
+          debug("Rotar Izquierda. seq#: ");
+          debugln(sequenceNumber);
+          // mover havia la izquierda
+          sendCommand(commands.move, sequenceNumber++, 1, valorDrone(-0.5), 0, 0, 0);
+        }
+
+        else {
+          // *******************************
+          // ningun boton apretado
+          debugln("volando -> ningun botón apretado -> ");
+          debug("Hovering. seq#: ");
+          debugln(sequenceNumber);
+          // HOVER
+          sendCommand(commands.move, sequenceNumber++, 0, 0, 0, 0, 0);
+        }
       }
 
-      else if (arr_state) {
-        // *******************************
-        // boton arriba
-        debug("volando - > boton arriba apretado -> ");
-        debug("Subir. seq#: ");
-        // comando para subir
-        debugln(sequenceNumber);
-      }
 
-      else if (aba) {
-        // *******************************
-        // boton abajo
-        debug(" volando - > boton abajo apretado -> ");
-        debug("Bajar. seq#: ");
-        sendCommand(commands.emergency, sequenceNumber++);
-        debugln(sequenceNumber);
-      }
-
-      else if (der) {
-        // *******************************
-        // boton derecha
-        debugln("boton derecha apretado -> ");
-      }
-
-      else if (izq) {
-        // *******************************
-        // boton izquierda
-        debugln("boton izq apretado -> ");
-      }
     } else {
       // *******************************
-      // MODO suelto
-      if (btn) {
-        // *******************************
-        // boton apretado
-        debug("boton apretado -> ");
-        flying = false;
-        debug("aterrizar. seq#: ");
-        sendCommand(commands.land, sequenceNumber++);
-        debugln(sequenceNumber);
-      }
-    }
-
-
-  } else {
-    // *******************************
-    // NOT FLYING
-    // *******************************
-    if (modo_state) {
+      // NOT FLYING
       // *******************************
-      // MODO apretado
-      if (btn) {
+      if (modo_state) {
         // *******************************
-        // boton apretado
-        debugln("boton apretado -> emergency.");
-        sendCommand(commands.emergency, sequenceNumber++);
-      }
-    } else {
-      // *******************************
-      // MODO suelto
-      if (btn) {
+        // MODO apretado
+        if (btn) {
+          // *******************************
+          // boton apretado
+          debugln("boton apretado -> emergency.");
+          sendCommand(commands.emergency, sequenceNumber++);
+        }
+      } else {
         // *******************************
-        // boton apretado
-        debug("boton apretado -> ");
-        flying = true;
-        debug("despegar. seq#: ");
-        sendCommand(commands.takeoff, sequenceNumber++);
-        debugln(sequenceNumber);
-      }
+        // MODO suelto
+        if (btn) {
+          // *******************************
+          // boton apretado
+          debug("boton apretado -> ");
+          flying = true;
+          debug("despegar. seq#: ");
+          sendCommand(commands.takeoff, sequenceNumber++);
+          debugln(sequenceNumber);
+        }
 
-      else if (arr) {
-        // *******************************
-        // boton arriba
-        debug("no volando - > boton arriba apretado -> ");
-        debugln("Set Alarm OK.");
-        sendCommand(commands.setAlarmOk, sequenceNumber++);
-        sendCommand(commands.flatTrim, sequenceNumber++);
-      }
+        else if (arr) {
+          // *******************************
+          // boton arriba
+          debug("no volando - > boton arriba apretado -> ");
+          debugln("Set Alarm OK.");
+          sendCommand(commands.setAlarmOk, sequenceNumber++);
+          sendCommand(commands.flatTrim, sequenceNumber++);
+        }
 
-      else if (aba) {
-        // *******************************
-        // boton abajo
-        debug("no volando - > boton abajo apretado -> ");
-        debug("Emergency. seq#: ");
-        sendCommand(commands.emergency, sequenceNumber++);
-        debugln(sequenceNumber);
-      }
+        else if (aba) {
+          // *******************************
+          // boton abajo
+          debug("no volando - > boton abajo apretado -> ");
+          debug("Emergency. seq#: ");
+          sendCommand(commands.emergency, sequenceNumber++);
+          debugln(sequenceNumber);
+        }
 
-      else if (der) {
-        // *******************************
-        // boton derecha
-        debugln("boton derecha apretado -> ");
-      }
+        else if (der) {
+          // *******************************
+          // boton derecha
+          debugln("boton derecha apretado -> ");
+        }
 
-      else if (izq) {
-        // *******************************
-        // boton izquierda
-        debugln("boton izq apretado -> ");
+        else if (izq) {
+          // *******************************
+          // boton izquierda
+          debugln("boton izq apretado -> ");
+        }
       }
     }
   }
+
 }
 
